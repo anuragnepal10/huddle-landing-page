df4febb576a78830a7a4b507f261a1505be4483b
anuragnepal10 <anuragnepal02@gmail.com>
2023-04-30 22:26:21 +0545

    completed desktop design

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,26 @@ export default {
 				custom: 'rgba(0, 0, 0, 0.1) 4px 4px 20px',
 			},
 		},
+		screens: {
+			sm: '640px',
+			md: '768px',
+			lg: '1080px',
+			xl: '1440px',
+		},
 		container: {
 			center: true,
 			padding: {
 				DEFAULT: '1rem',
 				sm: '2rem',
-				md: '2rem',
-				lg: '3rem',
-				xl: '3rem',
-				'2xl': '3rem',
+				md: '3rem',
+				lg: '4rem',
+				xl: '5rem',
+			},
+			screens: {
+				sm: '640px',
+				md: '640px',
+				lg: '640px',
+				xl: '1440px',
 			},
 		},
 		fontFamily: {
